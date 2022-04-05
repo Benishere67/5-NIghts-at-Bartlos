d3e93df4d47f62535c25dbeb7230b92b48b49c66
Sam <samscarbrough123@gmail.com>
2022-04-05 12:59:56 -0700

    Generation script started

---
+++ Assets/Scripts/Generation/RoomGenerator.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomGenerator : MonoBehaviour
+{
+    public GameObject key;
+    public GameObject door;
+    public GameObject cabinet;
+    public GameObject chair;
+    public GameObject table;
+    public GameObject teacherDesk;
+    public GameObject poster;
+    public GameObject whiteboard;
+    public GameObject lightSwitch;
+
+    public GameObject logicSwitch;
+    public GameObject pressurePlate;
+    public GameObject ghostSensor;
+
+
+    void Start()
+    {
+        
+    }
+
+
+}
