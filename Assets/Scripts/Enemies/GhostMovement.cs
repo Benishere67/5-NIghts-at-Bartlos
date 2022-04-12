450587e6adb9470b8ea568227f3dbf5e28d43798
Adrian McAllister <amcallister2317@student.pps.net>
2022-04-12 13:48:53 -0700

    Bartlo Ghost Navigation
    Added wandering and player tracking to the Bartlo Ghost

--- Assets/Scripts/Enemies/GhostMovement.cs
+++ Assets/Scripts/Enemies/GhostMovement.cs
@@ -6,17 +6,62 @@ public class GhostMovement : MonoBehaviour
 {
     public UnityEngine.AI.NavMeshAgent ghost;
     public GameObject player;
+    public float ghostMode;
+    public float wanderX;
+    public float wanderZ;
+    public Vector3 wanderDestination;
+    public bool wanderReset;
     
     void Start()
     {
+        wanderReset = false;
+        InvokeRepeating("WanderTimer", 0f, 2f);
         ghost = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
+        ghostMode = 0;
         player = GameObject.Find("Player");
         ghost.SetDestination(player.transform.position);
     }
 
+    void WanderTimer()
+    {
+        wanderReset = false;
+    }
+
+    void Wandering()
+    {
+        wanderX = ghost.transform.position.x + Random.Range(-5, 5);
+        wanderZ = ghost.transform.position.y + Random.Range(-5, 5);
+        wanderDestination = new Vector3(wanderX, 0, wanderZ);
+        ghost.SetDestination(wanderDestination); 
+        wanderReset = true;
+    }
+
+    void PlayerDetection()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(ghost.transform.position, 15);
+        for (int i = 0; i < hitColliders.Length; i++)
+        {
+            GameObject hitCollider = hitColliders[i].gameObject;
+            if (hitCollider.CompareTag("Player"))
+            {
+                ghostMode = 1;
+            }
+        }
+    }
 
     void Update()
     {
-        ghost.SetDestination(player.transform.position);
+        if (ghostMode == 0) {
+            Debug.Log("Wandering");
+            if (wanderReset == false) {
+                Wandering();
+            }
+            PlayerDetection();
+        } 
+        else if (ghostMode == 1) {
+            Debug.Log("Found the player");
+            ghost.SetDestination(player.transform.position);
+        }
+        
     }
 }
