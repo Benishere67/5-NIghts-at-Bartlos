25cda5f1d0439f7c987e250d69f40c12ca4acc4a
Sam <samscarbrough123@gmail.com>
2022-04-19 13:50:39 -0700

    Generate 1x1 square

--- Assets/Scripts/Generation/RoomGenerator.cs
+++ Assets/Scripts/Generation/RoomGenerator.cs
@@ -26,11 +26,37 @@ public class RoomGenerator : MonoBehaviour
     public GameObject ghostSensor;
     public GameObject magnetBox;
 
+    Mesh mesh;
+    Vector3[] vertices;
+    int[] triangles;
 
     void Start()
     {
-        
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+        createShape();
+        UpdateMesh();
     }
-
+   
+    void createShape(){
+        vertices = new Vector3[]
+        {
+            new Vector3(0,0,0),
+            new Vector3(0,0,1),
+            new Vector3(1,0,0),
+            new Vector3(1,0,1)
+        };
+        triangles = new int[]
+        {
+            0, 1, 2,
+            1, 3, 2
+        };
+    }
+    void UpdateMesh(){
+        mesh.Clear();
+        mesh.vertices=vertices;
+        mesh.triangles=triangles;
+        mesh.RecalculateNormals();
+    }
 
 }
