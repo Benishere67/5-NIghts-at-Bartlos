c795172c042c6c458f348b6756022bba62681932
Sam <samscarbrough123@gmail.com>
2022-04-21 14:08:20 -0700

    gen script plane

--- Assets/Scripts/Generation/RoomGenerator.cs
+++ Assets/Scripts/Generation/RoomGenerator.cs
@@ -6,9 +6,9 @@ using UnityEngine;
 
 public class RoomGenerator : MonoBehaviour
 {
-    public GameObject floor;
-    public GameObject wall;
-    public GameObject ceiling;
+    public Mesh floor;
+    public Mesh wall;
+    public Mesh ceiling;
 
     public GameObject key;
     public GameObject door;
@@ -26,37 +26,55 @@ public class RoomGenerator : MonoBehaviour
     public GameObject ghostSensor;
     public GameObject magnetBox;
 
-    Mesh mesh;
+    public float width;
+    public float depth;
+    public float height;
+    
+
+    Mesh[] meshes;
     Vector3[] vertices;
     int[] triangles;
 
     void Start()
     {
-        mesh = new Mesh();
-        GetComponent<MeshFilter>().mesh = mesh;
-        createShape();
+        
+        width = Random.Range(3,10);
+        depth = Random.Range(3,10);
+        height = Random.Range(10,15);
+        createRoom(width, height, depth);
+    }
+
+    void createRoom(float w, float h, float d){
+        floor = createShape(w, d);
+        UpdateMesh();
+        
+        ceiling = createShape(d, h);
         UpdateMesh();
     }
    
-    void createShape(){
+    Mesh createShape(float x, float y){
+        meshes[0] = new Mesh();
+        GetComponent<MeshFilter>().mesh = meshes[0];
         vertices = new Vector3[]
         {
             new Vector3(0,0,0),
-            new Vector3(0,0,1),
-            new Vector3(1,0,0),
-            new Vector3(1,0,1)
+            new Vector3(0,0,y),
+            new Vector3(x,0,0),
+            new Vector3(x,0,y)
         };
         triangles = new int[]
         {
             0, 1, 2,
             1, 3, 2
         };
+        Debug.Log(x + " by " + y);
+        return(meshes[0]);
     }
     void UpdateMesh(){
-        mesh.Clear();
-        mesh.vertices=vertices;
-        mesh.triangles=triangles;
-        mesh.RecalculateNormals();
+        meshes[0].Clear();
+        meshes[0].vertices=vertices;
+        meshes[0].triangles=triangles;
+        meshes[0].RecalculateNormals();
     }
 
 }
