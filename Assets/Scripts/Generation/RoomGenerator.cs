44ba73267e4f46b598cc2e089ae3d2b518d82290
Sam <samscarbrough123@gmail.com>
2022-04-29 14:13:36 -0700

    Generate room and trashcan

--- Assets/Scripts/Generation/RoomGenerator.cs
+++ Assets/Scripts/Generation/RoomGenerator.cs
@@ -23,6 +23,9 @@ public class RoomGenerator : MonoBehaviour
     public GameObject whiteboard;
     public GameObject ceilingLight;
     public GameObject lightSwitch;
+    public GameObject trashCan;
+    public GameObject recycleBin1;
+    public GameObject recycleBin2;
 
     public GameObject logicSwitch;
     public GameObject pressurePlate;
@@ -34,58 +37,34 @@ public class RoomGenerator : MonoBehaviour
     public float height;
     
 
-    public Mesh[] meshes;
-    public Vector3[] vertices;
-    public int[] triangles;
-
     void Start()
     {
         width = Random.Range(30,60);
         depth = Random.Range(30,60);
         height = Random.Range(13,18);
+        roomSizing();
+        placeObject(trashCan);
+    }
+
+    public void roomSizing(){
+        // Scales and places floor, ceiling, walls
         Vector3 size= new Vector3(width,1,depth);
         floor.transform.localScale = size;
         ceiling.transform.localScale = size;
-        wall1.transform.position = new Vector3(wall1.transform.position.x, wall1.transform.position.y,-depth/2);
+        ceiling.transform.position = new Vector3(ceiling.transform.position.x,height,ceiling.transform.position.z);
+        wall1.transform.position = new Vector3(wall1.transform.position.x, height/2,-depth/2);
         wall1.transform.localScale = new Vector3(width,1,height);
-        wall3.transform.position = new Vector3(wall3.transform.position.x, wall3.transform.position.y,depth/2);
+        wall3.transform.position = new Vector3(wall3.transform.position.x, height/2,depth/2);
         wall3.transform.localScale = new Vector3(width,1,height);
-        wall2.transform.position = new Vector3(width/2, wall2.transform.position.y,wall2.transform.position.z);
-        wall2.transform.localScale = new Vector3(width,1,height);
-        wall4.transform.position = new Vector3(-width/2, wall4.transform.position.y,wall4.transform.position.z);
-        wall4.transform.localScale = new Vector3(width,1,height);
+        wall2.transform.position = new Vector3(width/2, height/2,wall2.transform.position.z);
+        wall2.transform.localScale = new Vector3(depth,1,height);
+        wall4.transform.position = new Vector3(-width/2, height/2,wall4.transform.position.z);
+        wall4.transform.localScale = new Vector3(depth,1,height);
+    }
+    
+    public void placeObject(GameObject obid){
+        float x = Random.Range(-width/2,width/2);
+        float z = Random.Range(-depth/2,depth/2);
+        Instantiate(obid, new Vector3(x,.5f,z), Quaternion.Euler(-90,0,0));
     }
 }
-/*
-    void createRoom(float w, float h, float d){
-        floor = createShape(w, d);
-        UpdateMesh();
-
-    }
-   
-    Mesh createShape(float x, float y){
-        meshes[0] = new Mesh();
-        GetComponent<MeshFilter>().mesh = meshes[0];
-        vertices = new Vector3[]
-        {
-            new Vector3(0,0,0),
-            new Vector3(0,0,y),
-            new Vector3(x,0,0),
-            new Vector3(x,0,y)
-        };
-        triangles = new int[]
-        {
-            0, 1, 2,
-            1, 3, 2
-        };
-        Debug.Log(x + " by " + y);
-        return(meshes[0]);
-    }
-    void UpdateMesh(){
-        meshes[0].Clear();
-        meshes[0].vertices=vertices;
-        meshes[0].triangles=triangles;
-        meshes[0].RecalculateNormals();
-    }
-*/
-
