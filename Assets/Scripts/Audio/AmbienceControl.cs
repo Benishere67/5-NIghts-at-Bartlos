dbf0a91a92eb712faf00b4311ecba9c486b37adf
Adrian McAllister <amcallister2317@student.pps.net>
2022-06-02 12:40:49 -0700

    Ambient Noises

--- Assets/Scripts/Audio/AmbienceControl.cs
+++ Assets/Scripts/Audio/AmbienceControl.cs
@@ -7,25 +7,49 @@ public class AmbienceControl : MonoBehaviour
     public AudioSource ambience;
     public AudioClip[] audioClips;
     public AudioClip ambienceClip;
+    public GameObject AmbientSound;
+    
+    public Vector3 ambienceStart;
+    public Vector3 tpLocation;
 
     public int clipSelect;
     public float randTime;
+    public bool clipPlay;
+
+    public float tpX;
+    public float tpZ;
+
+    void Start()
+    {
+        clipPlay = false;
+        AmbientSound.transform.position = ambienceStart;
+    }
 
     void SoundReset()
     {
         clipSelect = Random.Range(0, audioClips.Length);
         ambienceClip = audioClips[clipSelect];
         ambience.PlayOneShot(ambienceClip);
-        randTime = Random.Range(1, 5);
+        randTime = Random.Range(15, 30);
+        
+        tpX = Random.Range(-200, 200);
+        tpZ = Random.Range(-150, 150);
+        tpLocation = new Vector3(tpX, 80, tpZ);
+        AmbientSound.transform.position = tpLocation;
     }
 
-    /*void Wait()
-    {
-        yield WaitForSeconds(randTime);
-    }
 
     void Update()
     {
-        yield StartCoroutine("Wait");
-    } */
+        randTime -= 1 * Time.deltaTime;
+
+        if (randTime <= 0) {
+            clipPlay = true;
+        }
+
+        if (clipPlay == true) {
+            SoundReset();
+            clipPlay = false;
+        }    
+    } 
 }
